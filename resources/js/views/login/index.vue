479bf6561b172bfe11b9004454c345be5b3e16ae
ZhuFangfang <zffforgithub@gmail.com>
2020-01-10 00:38:53 +0800

    rm useless data

--- resources/js/views/login/index.vue
+++ resources/js/views/login/index.vue
@@ -33,8 +33,8 @@
         </el-button>
       </el-form-item>
       <div class="tips">
-        <span style="margin-right:20px;">Email: admin@laravue.dev</span>
-        <span>Password: laravue</span>
+        <span style="margin-right:20px;">Email: admin@admin.com</span>
+        <span>Password: admin</span>
       </div>
     </el-form>
   </div>
@@ -64,8 +64,8 @@ export default {
     };
     return {
       loginForm: {
-        email: 'admin@laravue.dev',
-        password: 'laravue',
+        email: 'admin@admin.com',
+        password: 'admin',
       },
       loginRules: {
         email: [{ required: true, trigger: 'blur', validator: validateEmail }],
